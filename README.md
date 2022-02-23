635573a4faf0be7f9b9c6d7b5a093cbb6514a166
Adam Gyulavari <adam@polygence.org>
2022-02-23 15:50:39 +0100

    update description

--- README.md
+++ README.md
@@ -8,18 +8,30 @@ Welcome to the coding challenge portion of the Thinkful Engineering interview! T
 
 We want you to create a simple application to track spendings.
 
-A spending has the followings properties: 
+A spending has the followings properties:
+
 - when it happened
 - how much did we spend
 - in what currency
 - for what/description
 
-This simple web API allows the client applications to:
+This simple web app allows the users to:
 
 - Create a new spending
 - List all the spendings
 - List all the spendings ordered by amount, date
 - List all the spendings filtered by currency
+
+A spending JSON object looks something like this:
+
+```json
+{
+  "description": "Mango",
+  "amount": 1200,
+  "spent_at": "2022-02-23T14:47:20.381Z",
+  "currency": "USD"
+}
+```
 
 ### Technical requirements
 
@@ -27,8 +39,31 @@ This simple web API allows the client applications to:
 - The backend solution can be written in any programming language
   - We have created a scaffolded Python/Flask and a Node.js/Express version
   - But you can start from scratch in any language
-- Use any SQL database to store data
-- Write tests and as clean code as possible
+- No need for any SQL database to store data, you can store the data in memory
+
+### Acceptance criteria
+
+```cucumber
+Given there are no existing spendings
+When I fill the description, currency and amount
+And I click the "Save" button
+Then I see my spending in the list
+And the spending is persisted on the server
+And the form is emptied
+
+Given that I have not filled the description or the amount
+When I click the "Save" button
+Then I see an error indicating the missing field
+
+Given there are existing spendings in the list
+When I change the selected ordering
+Then the list is reorder by the selection
+
+Given there are existing spendings in the list
+When I click on a currency filter button (HUF, USD, All)
+Then the list is filtered by that selection
+And shows only the matching spendings
+```
 
 ### What We Look For
 
@@ -39,9 +74,9 @@ This is an opportunity for you to show us what your best code looks like. While 
 - Unit tests
 - Error handling
 
+If there are any pre-existing patterns in the code that you don't like, feel free to change them!
+
 ### Not required, but nice to have
 
 - Use a web framework (eg. Django)
-- A React client application that can use the API. eg:
-
-
+- Persist data in an SQL based database
--- web/src/components/SpendingList.js
+++ web/src/components/SpendingList.js
@@ -1,21 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { FiDollarSign } from 'react-icons/fi';
-import { DateTime } from 'luxon';
-import Loader from './Loader';
-import { ErrorMessage, Spending, IconWrapper, TextWrapper, Amount, AmountWrapper } from '../styles/ComponentStyles';
+import React, { useState, useEffect } from "react";
+import { FiDollarSign } from "react-icons/fi";
+import { DateTime } from "luxon";
+import Loader from "./Loader";
+import {
+  ErrorMessage,
+  Spending,
+  IconWrapper,
+  TextWrapper,
+  Amount,
+  AmountWrapper,
+} from "../styles/ComponentStyles";
 
-export default function SpendingList({
-  spendings,
-  setSpendings,
-}) {
+export default function SpendingList({ spendings, setSpendings }) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
     fetch(`http://localhost:5000/spendings`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
     })
       .then(async (res) => {
         const body = await res.json();
@@ -27,12 +31,13 @@ export default function SpendingList({
       .then((response) => {
         if (response.status === 200) {
           setSpendings(response.body);
-          setLoading(false);
         }
       })
       .catch((err) => {
         console.error(err);
         setError(true);
+      })
+      .finally(() => {
         setLoading(false);
       });
   }, []);
@@ -41,41 +46,41 @@ export default function SpendingList({
 
   return (
     <>
-        {error && (
-          <ErrorMessage>
-            The server is probably down. Please try again later.
-          </ErrorMessage>
-        )}
-        {!spendings.length && !error && (
-          <h1 style={{ textAlign: 'center', marginTop: '4rem' }}>
-            Yay!{' '}
-            <span role='img' aria-label='jsx-a11y/accessible-emoji'>
-              🎉
-            </span>{' '}
-            No spendings!
-          </h1>
-        )}
-        {spendings.length > 0 &&
-          spendings.map((spending) => (
-            <Spending key={spending.id}>
-              <IconWrapper>
-                <FiDollarSign color='var(--color-blue)' />
-              </IconWrapper>
-              <TextWrapper>
-                <h3>{spending.description}</h3>
-                <p>
-                  {DateTime.fromISO(spending.spent_at).toFormat(
-                    't - MMMM dd, yyyy'
-                  )}
-                </p>
-              </TextWrapper>
-              <AmountWrapper>
-                <Amount currency={spending.currency}>
-                  {(spending.amount / 100).toFixed(2)}
-                </Amount>
-              </AmountWrapper>
-            </Spending>
-          ))}
+      {error && (
+        <ErrorMessage>
+          The server is probably down. Please try again later.
+        </ErrorMessage>
+      )}
+      {!spendings.length && !error && (
+        <h1 style={{ textAlign: "center", marginTop: "4rem" }}>
+          Yay!{" "}
+          <span role="img" aria-label="jsx-a11y/accessible-emoji">
+            🎉
+          </span>{" "}
+          No spendings!
+        </h1>
+      )}
+      {spendings.length > 0 &&
+        spendings.map((spending) => (
+          <Spending key={spending.id}>
+            <IconWrapper>
+              <FiDollarSign color="var(--color-blue)" />
+            </IconWrapper>
+            <TextWrapper>
+              <h3>{spending.description}</h3>
+              <p>
+                {DateTime.fromISO(spending.spent_at).toFormat(
+                  "t - MMMM dd, yyyy"
+                )}
+              </p>
+            </TextWrapper>
+            <AmountWrapper>
+              <Amount currency={spending.currency}>
+                {(spending.amount / 100).toFixed(2)}
+              </Amount>
+            </AmountWrapper>
+          </Spending>
+        ))}
     </>
   );
 }
