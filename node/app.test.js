5574074da2774f46224daeb8e95b56b8be013a46
Adam Gyulavari <adam@polygence.org>
2022-02-23 15:39:36 +0100

    add simple jest test

--- node/app.test.js
+++ node/app.test.js
@@ -1,7 +1,13 @@
+const request = require("supertest");
 const app = require("./app");
 
-describe("spendings", () => {
-    it("returns a list of spendings", () => {
-        expect(app.today()).toBe(new Date().toLocaleDateString());
-    });
+describe("/spendings", () => {
+  test("It should response the GET method", done => {
+    request(app)
+      .get("/spendings")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+        done();
+      });
+  });
 });
