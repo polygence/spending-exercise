b9f03288d3ed64c4970811e5bdf47f1efb4ec791
Kozma Szilveszter <szilveszter.kozma@itron.com>
2022-07-25 21:19:57 +0200

    Init rails app

--- python/tests.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-
-def test_today():
-    with app.test_client() as cli:
-        resp = cli.get('/spendings')
-        assert resp.status_code == 200
-        assert len(resp.json) == 1
